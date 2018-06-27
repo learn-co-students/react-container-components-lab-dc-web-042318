0da3057169a18b32c873619f705bee858334a82d
joshpson <jpearson846@gmail.com>
2018-06-27 15:23:51 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,19 @@
 // Code MovieReviews Here
+import React from "react";
+
+const MovieReviews = props => (
+  <div className="review-list">
+    {props.reviews.map(review => (
+      <div className="review" key={review.link.url}>
+        <h3>{review.display_title}</h3>
+        <p>{review.headline}</p>
+      </div>
+    ))}
+  </div>
+);
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,53 @@
 // Code SearchableMovieReviewsContainer Here
+import React from "react";
+import MovieReviews from "./MovieReviews";
+
+const NYT_API_KEY = "f98593a095b44546bf4073744b540da0";
+
+class SearchableMovieReviewsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+      reviews: []
+    };
+  }
+
+  trackSearchInput = e => {
+    this.setState({
+      searchTerm: e.target.value
+    });
+  };
+
+  search = e => {
+    e.preventDefault();
+
+    let URL =
+      "https://api.nytimes.com/svc/movies/v2/reviews/all.json?" +
+      `api-key=${NYT_API_KEY}&query=${this.state.searchTerm}`;
+    fetch(URL)
+      .then(res => res.json())
+      .then(response => this.setState({ reviews: response.results }));
+  };
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <h1>Searchable Reviews</h1>
+        <form onSubmit={this.search}>
+          <input
+            placeholder="Search for a film"
+            value={this.state.searchTerm}
+            onChange={this.trackSearchInput}
+          />
+          <button type="submit" onClick={this.searchTerm}>
+            Search
+          </button>
+        </form>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}
+
+export default SearchableMovieReviewsContainer;
