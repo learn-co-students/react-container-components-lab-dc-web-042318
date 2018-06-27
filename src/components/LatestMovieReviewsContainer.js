d2acff525db9ebbde5f6208b79b0a8983550b647
Adam Cohn <adamcohn88@gmail.com>
2018-06-27 14:52:47 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews';
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?' + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+class LatestMovieController extends React.Component {
+  constructor() {
+    super();
+    this.state = { reviews: [] };
+  }
+
+  componentDidMount = () => {
+    fetch(URL)
+      .then(res => res.json())
+      .then(reviews => this.setState({ reviews: reviews.results }));
+  };
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <h1>Latest Reviews</h1>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}
+export default LatestMovieController;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,40 @@
-// Code SearchableMovieReviewsContainer Here
+import React from 'react';
+import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews';
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const SEARCHURL =
+  'https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=' + `api-key=${NYT_API_KEY}`;
+
+class SearchableMovieReviewsContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchTerm: '',
+      reviews: [],
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ searchTerm: event.target.value }, this.fetchReviews);
+  };
+
+  fetchReviews = () => {
+    fetch(`${SEARCHURL}${this.state.searchTerm}`)
+      .then(res => res.json())
+      .then(reviews => this.setState({ reviews: reviews }));
+  };
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <input type="text" onChange={this.handleChange} value={this.state.searchTerm} />
+        <h2>Search Results</h2>
+        <div>
+          <MovieReviews reviews={this.state.reviews} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchableMovieReviewsContainer;
