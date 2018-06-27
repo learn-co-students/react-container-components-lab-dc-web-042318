0b8346fef5f8c13c80bc841c1ba665d30ffa5d03
eckmLJE <lucas.eckman@gmail.com>
2018-06-27 16:32:09 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,21 @@
 // Code MovieReviews Here
+
+import React from "react";
+
+const Review = ({ headline, byline, link, summary_short }) => {
+  return (
+    <div key={headline} className="review">
+      {headline}
+    </div>
+  );
+};
+
+const MovieReviews = ({ reviews }) => {
+  return <div className="review-list">{reviews.map(Review)}</div>;
+};
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,45 @@
 // Code SearchableMovieReviewsContainer Here
+
+import React, { Component } from "react";
+import MovieReviews from "./MovieReviews.js";
+
+const NYT_API_KEY = "f98593a095b44546bf4073744b540da0";
+const URL =
+  "https://api.nytimes.com/svc/movies/v2/reviews/all.json?" +
+  `api-key=${NYT_API_KEY}&query=`;
+
+class SearchableMovieReviewsContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reviews: [],
+      searchTerm: ""
+    };
+  }
+
+  handleInput = e => {
+    this.setState({ searchTerm: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    fetch(URL + this.state.searchTerm)
+      .then(res => res.json())
+      .then(revObj => this.setState({ reviews: revObj.results }));
+  };
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <h3>SearchableMovieReviewsContainer</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleInput} />
+          <button>Search</button>
+        </form>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}
+
+export default SearchableMovieReviewsContainer;
